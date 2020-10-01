764dc5abb5ae28a03b44bd9b2ec7220a6eb58a55
jchang274 <jchang274@outlook.com>
2020-10-01 16:11:04 +0800

    Delet Chinese Words

--- TTSmain.py
+++ TTSmain.py
@@ -5,7 +5,7 @@ from azure.cognitiveservices.speech import SpeechSynthesisOutputFormat
 from azure.cognitiveservices.speech.audio import AudioOutputConfig
 import os
 import time
-time_now = time.strftime("%Y%m%d-%H%M-%S", time.localtime())
+time_now = time.strftime("%Y.%m.%d.%H%M%S", time.localtime())
 KEY = 'dece00af114f42a8b6c7324dca4d4125'
 REGION = "southeastasia"
 speech_config = SpeechConfig(subscription=KEY, region=REGION)
@@ -15,10 +15,9 @@ audio_config = AudioOutputConfig(filename=r'Cache\ConvertCache.mp3')
 synthesizer = SpeechSynthesizer(speech_config=speech_config, audio_config=audio_config)
 ssml_string = open("InputText.xml", "r").read()
 synthesizer.speak_ssml_async(ssml_string)
-
 filename1 = r'Cache\ConvertCache.mp3'
 filename2 = r'AudioOutput\Audio'+time_now+'.mp3'
-os.system('copy %s %s' % (filename1, filename2)) 
-os.startfile(r'AudioOutput\Audio'+time_now+'.mp3')
+os.system('copy %s %s' % (filename1, filename2))
+os.startfile(filename2)
 start_directory = r'AudioOutput'
 os.startfile(start_directory)
